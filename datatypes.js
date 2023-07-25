562aff5f75d4cf332bce3dc65123db00fffaa0f8
Donna Njoki <njerinjoki2@gmail.com>
2023-07-25 12:57:45 +0300

    Javascript short notes

---
+++ datatypes.js
@@ -0,0 +1,14 @@
+/* Javascript has 7 primitive data types and 1 non-primitive data type
+the primitive data types include:
+
+1. number: which includes numbers of any kind either an integer or floating-point.
+2. bigInt: for integer numbers of arbitrary length
+3. string: for strings
+4. booleans: for true/false
+5. null: for unknown values
+6. undefined: for unassigned values
+7.symbol: for unique identifiers
+
+the non-primitive data types 
+8. object: for more complex data structures.
+ */
