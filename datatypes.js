99478d845c578b3e241b6d7743af537f067e0c37
Donna Njoki <njerinjoki2@gmail.com>
2023-07-27 23:21:52 +0300

    js notes

--- datatypes.js
+++ datatypes.js
@@ -12,3 +12,24 @@ the primitive data types include:
 the non-primitive data types 
 8. object: for more complex data structures.
  */
+
+//Numbers
+
+/*
+>> Numbers represent both integer and floating numbers
+>> Additionally there are "special numeric values" which too belong to this data type
+they include:  Infinity, -Infinity and NaN.
+
+*/
+
+//BigInt
+
+/* This data type is used since the number type cannot represent integer values lager than ((2^53)-1) or less or less than -((2^53)-1) for negatives.
+ >> a BigInt value is created by appending n to the end of the integer
+*/
+
+//String
+
+/*
+>> A string must be surrounded by quotes
+*/
