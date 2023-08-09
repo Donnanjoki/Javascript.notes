5e6d0afa7d4653597d5238e7d799a095a955d0f7
Donna Njoki <njerinjoki2@gmail.com>
2023-08-09 23:59:34 +0300

    Javascript notes

--- Asynchronous.js
+++ Asynchronous.js
@@ -14,25 +14,85 @@ or tap before they actually do anything.
 >. The invocation of the callback function you provide notifies you of the condition or the event, and sometimes, the invocation will include function arguments that 
 provide additional details.
 
-  >>> Forms of callback-based Asynchronous Programming suing client-side JavaScript and Node.
+  >>> Forms of callback-based Asynchronous Programming uing client-side JavaScript and Node.
 
   1]] Timers
->.
+
+>. This kind of  asynchrony kicks in when you want to run some code after a certain amount of time has elapsed.
+   
+Example we can do this with setTimeout() function
+     setTimeout(checkForUpdates, 6000);
+     Explanation: setTimeout is a function that you invoke to register your callback function and specify under what asynchronous conditions should be invoked.
+
+Example: //call checkForUpdates in one minute and then again every minute after that.
+     let updateIntervalId = setInterval(checkForUpdates, 60000);
+     
+     // setInterval() returns a value that we can use to stop the repeated
+    // invocations by calling clearInterval(). (Similarly, setTimeout()
+   // returns a value that you can pass to clearTimeout())
+
+     function stopCheckingForUpdates() {
+      clearInterval(updateInterval);
+     }
 
   2]] Events
->.
+
+>.Client-side Javascript programs are almost universally event-driven: hence rather than running some kind of predetermined computation, they typically wait for the user to 
+something and then respond to the user's actions.
+
+>. The web browser generates an event when the user presses a key on the keyboard, moves the mouse, clicks a mouse button, or touches a touchscreen device.
+>. Event-driven Javascript programs register callback functions for specified types of events in a specified context, and the web browser invokes those functions whenever the specified
+event occurs.
+
+>. These callback functions are called event handlers or event listeners, and are registered with
+addEventListener():
 
   3]] Network Events
->. 
+
+>. Client-side Javascript code can use XMLHttpRequest class plus callback functions to make HTTP requests and asynchronously handle the server's response when it arrives.
+>. In moderN client-side Javascript, XMLHttpRequest has been largely replaced by the fetch() API.
 
   4]] Callbacks and Events in Node
+
+>. The Node.js server-side Javascript environment is deeply asynchronous and defines many APIs that use callbacks and events.
+    
+    Example 1: The default API for reading the contents of a file, for example, is asynchronous and invokes a callback function 
+    when the contents of the file have been read.
+
+        const fs = require("fs");  // the "fs" module has filesystem-related APIs
+        let options = {            // An object to hold options for our program
+
+            // default options would go here
+        };
+
+        // Read a configuration file then call the callback function
+        fs.readFile("config.json", "utf-8", (err, text) => {
+        if (err) {
+          // if there was an error, display the warning but continue
+          console.warn("Could not read config file", err);
+        } else {
+          // Otherwise, parse the file contents and assign to the options object
+          Object.assign(options, JSON.parse(text));
+        }
+
+         // In either case, we can now start running the program
+        startProgram(options);
+      });
+
+      Explanation: The Node's fs.readFile() function takes a two-parameter callback as its last argument.
+      >. It reads the specified file asynchronously and then invokes the callback. If the file was read successfully, it passes the file contents as the second callback argument.
+      >. If there was an error, it passes the error as the first callback argument
+
+
+          ### Promises ###
+>. A Promise is an object that represents the result of an asynchronous computation.
+
+
+          ### async and await ###
 >. 
 
-
-
-
-
-
+          ### Asynchronous Iteration ###
+>.
 
 
 
