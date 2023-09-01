406179120bb6d603c2cc1293843d7650fbbe08c1
Donna Njoki <njerinjoki2@gmail.com>
2023-09-01 20:37:11 +0300

    modules

--- ModernTutorial.js/modules.js
+++ ModernTutorial.js/modules.js
@@ -1,7 +1,7 @@
 /*
                           #### Modules ####
 
-        *** Intro ***
+        *** Intro *** PART A
 
 >. The goal of modular programming is to allow large programs to be assembled using modules of code from disparate authors
 and sources.
@@ -60,17 +60,69 @@ automatically fetch and evaluate the imported module and run the script.
 
    3]] A module code is evaluated only the first time when imported
 
+>. If the same module is imported into multiple other modules, it code is executed only once, upon the first import.
+Then all its exports are given further importers.
+
+>. Rule: Top-level module code should be used for initialization, creation of module-specific internal data structures.
+>. If we need to make something callable multiple times, then we should export it as a function.
+
+
+       >>>> import.meta
+
+>. The object import.meta meta-property exposes context-specific metadata to a Javascript module.
+It contains information about the module such as the module's URL.
+
 
    4]] In a module "this" is undefined
 
->. In a module, top-level this is undefined
+>. In a module, top-level (this) is undefined, unlike non-module scripts where (this) is a global object.
+
+
+        ### Browser-specific Features
+
+    A]] Module scripts are deferred
+
+    B]] Async works on inline scripts
+
+    C]] External Scripts
+
+    D]] No "bare" modules allowed
+
+    E]] Compatibility, "nomodule"
 
 
 
+         ### Build Tools ###
+
+>. In real-life browser modules are rarely used in their raw form, as they are bundled together 
+with tools such as webpack and later on deployed.
+
+>. Among the many benefits of using bundlers is that they give us control over how modules are resolved, allowing bare modules and 
+other such as CSS/HTML modules.
 
 
+>. Build tools do the following; 
+     
+     A]] Take a "main" module, the one intended to be put in <script type="module"> in HTML.
+    
+     B]] Analyze its dependencies; imports and then imports of imports etc.
 
+     C]] Build a single file with all the modules (or multiple files, that tunable), replacing native import calls'
+     with bundler functions, so that it works.  "Special" module types like HTML/CSS modules are also supported.
+      
+     D]] Additionally other transformations and optimizations may be applied; such as 
+             >. Unreachable code removed
+             >. Unused exports removed ("tree-shaking")
+             >. Development-specific statements like console and debugger removed.
+             >. Modern, bleeding-edge JS syntax may be transformed to older one with similar functionality using babel.
+             >. The resulting file is minified (spaces removed, variables replaced with shorter names. etc)
 
+>. If we use bundle tools, then scripts are bundled together into a single file(or a few file), import/export statements
+inside those scripts are replaced by special bundler functions, hence the resulting "bundled" script does not contain any import/export,
+it doesn't require type = "module", and can be put into a regular script as so
+
+      Example: Assuming we're using a tool such as webpack
+            <script src="bundle.js"></script>
 
 
 
@@ -90,5 +142,42 @@ automatically fetch and evaluate the imported module and run the script.
 >. When using modules, each module implements the functionality and exports it. Then we use import to directly 
 import it where its needed. The browsers loads and evaluates the scripts automatically.
 > Webpack is used to bundle modules together for performance and manage dependency relationships in projects.
+
+
+
+             *** Export and Import *** >>> PART B
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 */
